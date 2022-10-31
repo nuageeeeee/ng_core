57899ef8fd995e77227182643eaf21cedb6d0c9f
nuageeeeee <106245123+nuageeeeee@users.noreply.github.com>
2022-10-31 16:02:06 +0100

    create playerDroppedSave event

--- server/server.lua
+++ server/server.lua
@@ -2,8 +2,14 @@
 local NX = exports['NX']:useServer()
 -- NetEvent
 RegisterNetEvent('saveAll')
+RegisterNetEvent('playerDroppedSave')
 -- saveAll loop
 AddEventHandler('saveAll', function()
     NX.Players.SaveAll()
     print('succesful')
+end)
+
+AddEventHandler('playerDroppedSave', function()
+    local nxPlayer = NX.Players.Get(source)
+    nxPlayer.Save()
 end)
