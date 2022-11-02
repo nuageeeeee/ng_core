ba73c105224bfa1a32ed407e5a3eaff8450979c1
nuageeeeee <106245123+nuageeeeee@users.noreply.github.com>
2022-11-02 18:45:20 +0100

    ??

--- client/player/createchar/createcharacter.lua
+++ client/player/createchar/createcharacter.lua
@@ -123,12 +123,18 @@ end
 
 function charCreator()
     local uidP = NX.Player.GetValue('uid')
+    local IndexList = 1
     local charcrea = RageUI.CreateMenu("Creation personnage", "uid : " .. uidP .. " Prenom :" .. identity.firstName)
     RageUI.Visible(charcrea, not RageUI.Visible(charcrea))
     while charcrea do
         Citizen.Wait(0)
         RageUI.IsVisible(charcrea, true, true, true, function()
-            RageUI.Slider('Tete', '0', '10', Description, Divider, Style, Enabled, Actions)
+            RageUI.List("Tete : ", {"Tete1", "Tete2"}, IndexList, nil, {}, true,
+                function(Hovered, Active, Selected, Index)
+                end, function(Index, Items)
+                    IndexList = index
+                    print(index)
+                end)
         end, function()
         end)
 
