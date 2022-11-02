01828459a36920d8e6e436eb3ff3532e522934ee
nuageeeeee <106245123+nuageeeeee@users.noreply.github.com>
2022-11-02 15:37:28 +0100

    Update dbchar working now!!

--- server/dbchar.lua
+++ server/dbchar.lua
@@ -1,16 +1,17 @@
 local NX = exports['NX']:useServer()
 
-RegisterNetEvent('charAccept')
-AddEventHandler('charAccept', function(Identity)
-    local nxPlayer = NX.Players.Get(1)
-    local Firstame = Identity.FirstName
-    local Lastname = Identity.LastName
-    local dateOfBirth = Identity.DOB
-    local Sex = Identity.Sex
-    local Taille = Identity.Taille
-    nxPlayer.setCharInfoKey('firstname', Firstame)
-    nxPlayer.setCharInfoKey('lastname', Lastname)
-    nxPlayer.setCharInfoKey('dob', dateOfBirth)
-    nxPlayer.setCharInfoKey('sex', Sex)
-    nxPlayer.setCharInfoKey('height', Taille)
+RegisterNetEvent('ng_core:characcept')
+AddEventHandler('ng_core:characcept', function(Identity)
+    local nxPlayer = NX.Players.Get(source)
+    local Firstname = Identity.firstName
+    local Lastname = Identity.lastName
+    local dateOfBirth = Identity.dateOfBirth
+    local Sex = Identity.sex
+    local Taille = Identity.taille
+    print(Firstname)
+    nxPlayer.SetCharInfoKey('firstname', Firstname)
+    nxPlayer.SetCharInfoKey('lastname', Lastname)
+    nxPlayer.SetCharInfoKey('dob', dateOfBirth)
+    nxPlayer.SetCharInfoKey('sex', Sex)
+    nxPlayer.SetCharInfoKey('height', Taille)
 end)
