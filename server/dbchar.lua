a0b12dd0c3732a1acfa171a37c38eded97835580
nuageeeeee <106245123+nuageeeeee@users.noreply.github.com>
2022-11-02 01:51:05 +0100

    Maybe i will change this later

--- server/dbchar.lua
+++ server/dbchar.lua
@@ -3,6 +3,14 @@ local NX = exports['NX']:useServer()
 RegisterNetEvent('charAccept')
 AddEventHandler('charAccept', function(Identity)
     local nxPlayer = NX.Players.Get(1)
-    local FirstName = Identity.FirstName
-    nxPlayer.setCharInfoKey(firstname, 'deb')
+    local Firstame = Identity.FirstName
+    local Lastname = Identity.LastName
+    local dateOfBirth = Identity.DOB
+    local Sex = Identity.Sex
+    local Taille = Identity.Taille
+    nxPlayer.setCharInfoKey('firstname', Firstame)
+    nxPlayer.setCharInfoKey('lastname', Lastname)
+    nxPlayer.setCharInfoKey('dob', dateOfBirth)
+    nxPlayer.setCharInfoKey('sex', Sex)
+    nxPlayer.setCharInfoKey('height', Taille)
 end)
